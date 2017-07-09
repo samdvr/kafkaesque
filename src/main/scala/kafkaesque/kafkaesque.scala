cd53f5c08568ea052e2e21319f2f6906e8f2ee4c
Sam Davarnia <samdvr@users.noreply.github.com>
2017-07-09 14:34:38 -0700

    Update kafkaesque.scala

--- src/main/scala/kafkaesque/kafkaesque.scala
+++ src/main/scala/kafkaesque/kafkaesque.scala
@@ -1,13 +1,29 @@
-
+import io.circe.{Encoder}
+import io.circe.generic.auto._
+import io.circe.syntax._
+import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord, RecordMetadata}
 import java.util.Properties
-
 import com.typesafe.config.ConfigFactory
-import kafkaesque.data.Event
-import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
+import java.util.concurrent.Future
 
 object Kafkaesque {
 
-  def kafkaConfig = {
+  implicit class Publisher[A](data: A) {
+
+    def publish(topic: String)(implicit encoder: Encoder[A]) = {
+      producer.send(new ProducerRecord[String, String](topic, data.asJson.noSpaces))
+    }
+  }
+
+  implicit class SeqPublisher[A](dataSeq: Seq[A]) {
+    def publish(topic: String)(implicit encoder: Encoder[A]): Seq[Future[RecordMetadata]] = {
+      dataSeq.map( data => producer.send(new ProducerRecord[String, String](topic, data.asJson.noSpaces)))
+    }
+  }
+
+  private lazy val producer = new KafkaProducer[String, String](kafkaConfig)
+
+  private lazy val kafkaConfig = {
     val props = new Properties()
     val conf = ConfigFactory.load()
     props.put("bootstrap.servers", conf.getString("kafkaesque.brokers"))
@@ -16,17 +32,5 @@ object Kafkaesque {
     props
 
   }
-
-  implicit class Publisher(events: Seq[Event]) {
-    def publish = {
-      val producer = new KafkaProducer[String, String](kafkaConfig)
-
-      val producedEvents = events.map {
-        event => producer.send(new ProducerRecord[String, String](event.topic, event.message))
-      }
-
-      producer.close()
-      producedEvents
-    }
-  }
+  
 }
